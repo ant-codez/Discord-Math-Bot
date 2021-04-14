fe6e557d205e127f165fe96f727d49105939a655
Provmawn <provmawn@gmail.com>
2021-04-14 00:51:58 -0700

    Make generate function return a string

--- src/equation_generator.py
+++ src/equation_generator.py
@@ -12,11 +12,11 @@ class EquationGenerator:
         }
 
     # Generates an equation with one of the operations as a parameter: +, /, *, -
-    def generate(self, operation: str): 
+    def generate(self, operation: str) -> str:
         op1: int = random.randint(1, 10)
         op2: int = random.randint(1, 10)
-        print("{} {} {} = ".format(op1, operation, op2))
         self.answer = self.operations[operation](op1, op2)
+        return "{} {} {} = ".format(op1, operation, op2)
 
     def check_answer(self, answer: str):
         return self.answer == int(answer)
