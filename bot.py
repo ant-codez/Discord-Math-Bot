39d238492f556cf0ce9ba7fe0bdc06155b05bd56
ant-codez <achavez@student.42.us.org>
2021-04-14 00:09:53 -0700

    updating file

--- bot.py
+++ bot.py
@@ -7,5 +7,19 @@ client = commands.Bot(command_prefix = '$')
 async def on_ready():
     print('Bot is ready');
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server')
+    
+@client.event   
+async def on_member_remove(member):
+    print(f'{member} has left a server')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+    
+    
 #token
-client.run('ODMxNzc3NzU5MTg0Mjg5Nzk5.YHaLZw.0FI4k_C9RS4ckO1Ir2jKxwJaY5g')
+client.run('ODMxNzc3NzU5MTg0Mjg5Nzk5.YHaLZw.0svbfRRPa6ZLlw5LZPQ8GRHPDrU')
